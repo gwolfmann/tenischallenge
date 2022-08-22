2a1b8d73d90b7fdd403cfbfacc4638a0a7f866f2
gwolfmann <gwolfmann@gmail.com>
2022-08-22 00:23:07 -0300

    tournamente model

--- model/src/main/java/com/tenistournement/model/tournamentModel/Tournament.java
+++ model/src/main/java/com/tenistournement/model/tournamentModel/Tournament.java
@@ -1,12 +1,23 @@
 package com.tenistournement.model.tournamentModel;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Data
+@ToString
+@JsonInclude
+@Builder
+@Document
+
 public class Tournament {
 
+        @Id
         private String idTournament;
         private PlayerDTO playerA;
         private PlayerDTO playerB;
